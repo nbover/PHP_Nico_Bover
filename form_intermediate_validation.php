3f1b8ad0387e8c042e4b5eae7bd07cb6de414432
nbover <nbover.07@gmail.com>
2020-11-19 20:41:08 +0100

    Validation

---
+++ form_intermediate_validation.php
@@ -0,0 +1,24 @@
+<?php
+  //Nicolau Martí Bover Armstrong
+    echo "<hr><h3>Nicolau Martí Bover Armstrong</h3><hr>";
+      echo "<h2>Funció del filter_var</h2>";
+        function emailValidation ($variable){
+          return filter_var($variable, FILTER_VALIDATE_EMAIL);
+        }
+
+        if (emailValidation ('nbover.07@gmail.com')){
+          echo 'VALID';
+        } else {
+          echo 'NOT VALID';
+        }
+      echo "<hr><h2>Funció del strcmp</h2>";
+        function passwordValidation ($variable1,$variable2){
+            if (strcmp($variable1,$variable2) == 0)
+              return True;
+            return False;
+        }
+
+        echo passwordValidation ("Hola","Hola");
+
+
+?>
