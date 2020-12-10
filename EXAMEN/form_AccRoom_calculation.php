1e833d3eece3213d6e5c13d1ccad2ba8d33ccacb
nbover <nbover.07@gmail.com>
2020-12-10 20:52:12 +0100

    EXAMEN

---
+++ EXAMEN/form_AccRoom_calculation.php
@@ -0,0 +1,54 @@
+<?php
+//calcular el preu del tipus d'habitació i retornar un valor numeric
+//si es single el preu es 30
+//si es double el preu es 50
+//en cas contrari el preu es 0
+//emprar la funcio strcmp vista als exercicis de classe
+function typePriceCalculation($type){
+
+  if (strcmp($type,'single') ==0)
+    return 30;
+  elseif (strcmp($type,'double') ==0)
+    return 50;
+  else
+    return 0;
+}
+
+//calcular el preu segons el genere d'habitació i retornar un valor numeric
+//si es female o male el preu es 10
+//si es mixed el preu es 5
+//en cas contrari el preu es 0
+//emprar la funcio strcmp vista als exercicis de classe
+function genderPriceCalculation($gender){
+  if (strcmp($gender,'female') !==1)
+    return 10;
+  elseif (strcmp($gender,'male') !==1)
+    return 10;
+  elseif (strcmp($gender,'mixed') !==1)
+    return 5;
+  else
+    return 0;
+}
+
+//calcular el preu segons les vistes d'habitació i retornar un valor numeric
+//si s'ha marcat amb vistes (yes) el preu es 15
+//en cas contrari el preu es 0
+//emprar la funcio strcmp vista als exercicis de classe
+function viewsPriceCalculation($views){
+  if (strcmp($views,'yes') !==1)
+    return 15;
+  else
+    return 0;
+}
+
+//calcular el preu segons els serveis d'habitació seleccionats i retornar un valor numeric
+//per cada servei seleccionat el preu s'incrementa 20
+//example: si s'han seleccionat 3 serveis de restauració el preu es 20 multiplicat per 3
+//si no s'ha seleccionat cap el preu es 0
+//emprar una de les dues funcions vistes a classe per a saber quants elements hi ha a un array
+
+function foodServicesPriceCalculation($foodServices){
+  return (sizeof($_GET['foodServices'])*20);
+}
+
+?>
---
+++ EXAMEN/vector_hash_algos.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Donada la funcio de php hash_algos()
+1-Mostra amb un bucle for each tots els seus elements
+2- de manera ordenada alfabeticament
+3- I mostra amb accés directe per posició als elements amb els valors:
+    sha256
+    md5
+mes informació al link:
+https://www.php.net/manual/en/function.hash-algos.php
+......
+*/
+//1.
+print_r(hash_algos());
+echo "<hr>";
+//2.
+$prova = hash_algos();
+asort($prova);
+print_r($prova);
+echo "<hr>";
+//3.
+foreach ($prova as $val1 => $val2) {
+  echo "$val2<br>";
+}
+ ?>
